0528567e4ece80e3de50a8042cd117440287a5e7
Geoffrey Prest <geoffrey.prest@cgi.com>
2019-02-27 16:14:04 -0500

    All tests broken II

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { OtherComponent } from './other/other.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
